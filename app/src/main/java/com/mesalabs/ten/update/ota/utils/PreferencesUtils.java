890fbccfe8ad14c37bee404382886e21693634f9
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-09-12 02:01:27 -0300

    Update PreferencesUtils.java

--- app/src/main/java/com/mesalabs/ten/update/ota/utils/PreferencesUtils.java
+++ app/src/main/java/com/mesalabs/ten/update/ota/utils/PreferencesUtils.java
@@ -161,8 +161,8 @@ public class PreferencesUtils {
         
         private static String NAME = "rom_name";
         private static String VERSION_NAME = "rom_version_name";
-        private static String PrevVer = "kernel_PrevVer";
-        private static String CurVer = "kernel_CurVer";
+        private static Int PrevVer = "kernel_PrevVer";
+        private static Int CurVer = "kernel_CurVer";
         private static String DOWNLOAD_URL = "rom_download_url";
         private static String MD5 = "rom_md5";
         private static String CHANGELOG_HEADER = "rom_changelog_header_img";
